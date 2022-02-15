6dffcf7d6af9a4dfa25f7ae0f4c3b051225b69d3
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-02-14 18:09:02 -0800

    move functions to file

--- src/sentry/integrations/jira/utils/__init__.py
+++ src/sentry/integrations/jira/utils/__init__.py
@@ -1,7 +1,9 @@
 from .api import get_assignee_email, handle_assignee_change, handle_status_change, set_badge
 from .choice import build_user_choice
+from .group import build_context
 
 __all__ = (
+    "build_context",
     "build_user_choice",
     "get_assignee_email",
     "handle_assignee_change",
--- src/sentry/integrations/jira/views/issue.py
+++ src/sentry/integrations/jira/views/issue.py
@@ -1,85 +1,19 @@
 from __future__ import annotations
 
 import logging
-from functools import reduce
-from typing import Any, Mapping
-from urllib.parse import quote
 
 from jwt import ExpiredSignatureError
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry.api.serializers import StreamGroupSerializer, serialize
 from sentry.integrations.utils import AtlassianConnectValidationError, get_integration_from_request
 from sentry.models import ExternalIssue, Group, GroupLink
-from sentry.utils.http import absolute_uri
 from sentry.utils.sdk import configure_scope
 
-from ..utils import set_badge
+from ..utils import build_context, set_badge
 from . import JiraBaseHook
 
 logger = logging.getLogger(__name__)
-
-
-def accum(tot: int, item: Mapping[int, int]) -> int:
-    return tot + item[1]
-
-
-# TODO: find more efficient way of getting stats
-def get_serialized_and_stats(group: Group, stats_period: str) -> tuple[Mapping[str, Any], int]:
-    result = serialize(
-        group,
-        None,
-        StreamGroupSerializer(stats_period=stats_period),
-    )
-    stats = result["stats"][stats_period]
-    return result, reduce(accum, stats, 0)
-
-
-def get_release_url(group: Group, release: str) -> str:
-    project = group.project
-    return absolute_uri(
-        "/organizations/{}/releases/{}/?project={}".format(
-            project.organization.slug, quote(release), project.id
-        )
-    )
-
-
-def get_group_url(group: Group) -> str:
-    return group.get_absolute_url(params={"referrer": "sentry-issues-glance"})
-
-
-def build_context(group: Group) -> Mapping[str, Any]:
-    result, stats_24hr = get_serialized_and_stats(group, "24h")
-    _, stats_14d = get_serialized_and_stats(group, "14d")
-
-    first_release = group.get_first_release()
-    if first_release is not None:
-        last_release = group.get_last_release()
-    else:
-        last_release = None
-
-    first_release_url = None
-    if first_release:
-        first_release_url = get_release_url(group, first_release)
-
-    last_release_url = None
-    if last_release:
-        last_release_url = get_release_url(group, last_release)
-
-    return {
-        "type": result.get("metadata", {}).get("type", "Unknown Error"),
-        "title": group.title,
-        "title_url": get_group_url(group),
-        "first_seen": result["firstSeen"],
-        "last_seen": result["lastSeen"],
-        "first_release": first_release,
-        "first_release_url": first_release_url,
-        "last_release": last_release,
-        "last_release_url": last_release_url,
-        "stats_24hr": stats_24hr,
-        "stats_14d": stats_14d,
-    }
 
 
 class JiraIssueHookView(JiraBaseHook):
